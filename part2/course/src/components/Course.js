a5c6702ff2e3e0875f1f5522391a3b9029837b2b
Shelakh <staffultimaunion@gmail.com>
2022-11-25 00:24:43 +0100

    refactor Course.js

--- part2/course/src/components/Course.js
+++ part2/course/src/components/Course.js
@@ -1,26 +1,27 @@
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Content = ({ course }) => {
+
+  return (
+    <div>
+      {course.parts.map((part) =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
 
 const Course = ({ course }) => {
-  const Header = () => {
-    return (
-      <h1>{course.name}</h1>
-    )
-  }
-  
-  const Content = () => {
-    const Part = ({ part }) => {
-      return (
-        <p>{part.name} {part.exercises}</p>
-      )
-    }
-    return (
-      <div>
-        {course.parts.map((part) =>
-          <Part key={part.id} part={part} />
-        )}
-      </div>
-    )
-  }
-  
   return (
     <div>
       <Header course={course} />
