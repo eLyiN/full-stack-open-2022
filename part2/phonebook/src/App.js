3fbb03b196c000ec0bf7dab81b532e448ee46fb8
Shelakh <staffultimaunion@gmail.com>
2022-11-28 01:14:48 +0100

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,21 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number : '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setFilter] = useState('')
 
   const addNewName = (event) => {
     event.preventDefault()
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       setNewName('')
+      setNewNumber('')
       return;
     }
     const nameObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(nameObject))
     setNewName('')
@@ -32,18 +38,30 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (e) => {
+    setFilter(e.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+          value={newFilter}
+          onChange={handleFilter}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           name:
           <input
             value={newName}
             onChange={handleNameChanges}
-          /><br/>
+          /><br />
           number:
-          <input 
+          <input
             value={newNumber}
             onChange={handleNumberChanges}
           />
@@ -53,10 +71,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => {
-        return (<div key={person.name} >
-          {person.name} {person.number}
-        </div>);
+      {persons.filter(
+        person => person.name.toLowerCase().includes(newFilter.toLowerCase())
+      ).map(filtered => {
+        return (
+          <div key={filtered.name} >
+            {filtered.name} {filtered.number}
+          </div>)
       })}
     </div>
   )
